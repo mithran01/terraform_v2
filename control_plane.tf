e964347cd1d733b5ff147e0fa584b77cdb5d1c47
mithran01 <mithran@capminds.com>
2026-05-11 18:59:48 +0530

    [main]:[fix]:[subnet_id added]

--- control_plane.tf
+++ control_plane.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "control_plane" {
   count                       = 1
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t3.micro"
-  subnet_id                   = data.aws_subnet.subnet_us_east_1a
+  subnet_id                   = data.aws_subnet.subnet_us_east_1a.id
   key_name                    = aws_key_pair.demov2.key_name
   associate_public_ip_address = true
   security_groups             = ["sg-0a77e32b49bdfe70e"]
