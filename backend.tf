39e8e8d853c5ac39c127f6286bc0ca0f73322c2d
mithran01 <mithran@capminds.com>
2026-05-11 18:40:23 +0530

    [main]:[fix]:[backend.tf]

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "mithran-tf-state"
-    key            = "ansible-server/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket = "mithran-tf-state"
+    key    = "ansible-server/terraform.tfstate"
+    region = "us-east-1"
   }
 }
