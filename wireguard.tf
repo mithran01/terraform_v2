43485a82dfff97f31fd949ae72aea4184d926936
mithran01 <mithran@capminds.com>
2026-05-15 11:47:44 +0530

    [main]:[provision]:[wireguard]

--- wireguard.tf
+++ wireguard.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "wireguardVPN" {
-  count                       = 0
+  count                       = 1
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t3.micro"
   subnet_id                   = data.aws_subnet.subnet_us_east_1a.id
