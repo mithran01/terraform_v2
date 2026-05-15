97a836f3ec9bf842683277674cca056f5b483513
mithran01 <mithran@capminds.com>
2026-05-15 19:22:45 +0530

    [main]:[infra]:[update]

--- data-plane.tf
+++ data-plane.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "data_plane" {
-  count                       = 2
+  count                       = 1
   ami                         = data.aws_ami.rocky_linux.id
   instance_type               = "t3.small"
   subnet_id                   = data.aws_subnet.subnet_us_east_1b.id
